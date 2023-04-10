c719ba04bb14c527aee9cd92ed0be33479908540
nauvalachid <nauvalachid@gmail.com>
2023-04-10 10:53:20 +0700

    Membuat method

--- class/class.cpp
+++ class/class.cpp
@@ -15,3 +15,12 @@ public: void cetak() {
 		  string kode;
 		  string namaM;
 		  int sks;
+	  public:
+		  void input() {
+			  cout << "Masukan Kode Matakuliah : ";
+			  cin >> kode;
+			  cout << "Masukan Nama Matakuliah : ";
+			  cin >> namaM;
+			  cout << "Jumlah SKS : ";
+			  cin >> sks;
+		  }
