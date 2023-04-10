0c0510126b1d13b3e7a440227120994cad650772
nauvalachid <nauvalachid@gmail.com>
2023-04-10 10:42:21 +0700

    Add project files.

---
+++ class/class.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class Mahasiswa {
+public:
+	string nim;
+	string nama;
+public: void cetak() {
+	cout << "NIM = " << nim;
+	cout << "\nNama = " << nama;
+};
+
+	  class Matakuliah {
+	  private:
+		  string kode;
+		  string namaM;
+		  int sks;
